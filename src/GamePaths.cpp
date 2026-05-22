b83f74f7227f09ad89bcd5649c822ae36906c769
DropSonic <130788813+DropSonic0@users.noreply.github.com>
2026-05-22 20:20:40 -0300

    Save Utility PS3

--- src/GamePaths.cpp
+++ src/GamePaths.cpp
@@ -4,6 +4,9 @@
 #include <SDL.h>
 #else
 #include <cell/cell_fs.h>
+#include <cell/sysmodule.h>
+#include <sysutil/sysutil_gamecontent.h>
+#include <sysutil/sysutil_common.h>
 #endif
 #ifdef __PS3__
 #include <stdio.h>
@@ -25,18 +28,34 @@
 #include <sys/stat.h>
 #endif
 
+#ifdef __PS3__
+extern "C" int cellSysmoduleLoadModule(uint16_t id);
+#endif
+
 namespace GamePaths
 {
 
 static char s_userPath[512] = "";
+#ifdef __PS3__
+static char s_savePath[512] = "";
+#endif
 
 void Init()
 {
+#ifdef __PS3__
+    cellSysmoduleLoadModule(0x003e); // CELL_SYSMODULE_SYSUTIL_GAME
+
+    // Assets are in USRDIR
+    ::snprintf(s_userPath, sizeof(s_userPath), "/dev_hdd0/game/TH06PORT0/USRDIR/");
+    // Default save path to asset path
+    ::snprintf(s_savePath, sizeof(s_savePath), "%s", s_userPath);
+#endif
+
 #ifdef __ANDROID__
     const char *internalPath = SDL_AndroidGetExternalStoragePath();
     if (internalPath)
     {
-        snprintf(s_userPath, sizeof(s_userPath), "%s/", internalPath);
+        ::snprintf(s_userPath, sizeof(s_userPath), "%s/", internalPath);
         SDL_Log("GamePaths: user data path = %s", s_userPath);
     }
     else
@@ -46,7 +65,74 @@ void Init()
         s_userPath[0] = '\0';
     }
 #elif defined(__PS3__)
-    snprintf(s_userPath, sizeof(s_userPath), "/dev_hdd0/game/TH06PORT0/USRDIR/");
+    // Initialize Game Utility for saves
+    {
+        CellGameSetInitParams init;
+        ::memset(&init, 0, sizeof(CellGameSetInitParams));
+        ::strncpy(init.title, "Touhou 06 Saves", CELL_GAME_SYSP_TITLE_SIZE);
+        ::strncpy(init.titleId, "TH06SAVES", CELL_GAME_SYSP_TITLEID_SIZE);
+        ::strncpy(init.version, "01.00", CELL_GAME_SYSP_VERSION_SIZE);
+
+        char contentInfoPath[128];
+        char usrdirPath[128];
+        ::memset(contentInfoPath, 0, 128);
+        ::memset(usrdirPath, 0, 128);
+        CellGameContentSize size;
+
+        cellSysutilCheckCallback();
+        // GAMETYPE_GAMEDATA is 3
+        int ret = cellGameDataCheck(3, init.titleId, &size);
+        
+        bool shouldPermit = (ret == 0);
+
+        if (ret != 0)
+        {
+            cellSysutilCheckCallback();
+            ret = cellGameCreateGameData(&init, contentInfoPath, usrdirPath);
+            if (ret == 0)
+            {
+                // Copy ICON0.PNG
+                char srcIcon[256], dstIcon[256];
+                ::snprintf(srcIcon, sizeof(srcIcon), "%sICON0.PNG", s_userPath);
+                ::snprintf(dstIcon, sizeof(dstIcon), "%s/ICON0.PNG", contentInfoPath);
+                int fsrc, fdst;
+                if (cellFsOpen(srcIcon, CELL_FS_O_RDONLY, &fsrc, NULL, 0) == CELL_FS_SUCCEEDED)
+                {
+                    if (cellFsOpen(dstIcon, CELL_FS_O_WRONLY | CELL_FS_O_CREAT | CELL_FS_O_TRUNC, &fdst, NULL, 0) ==
+                        CELL_FS_SUCCEEDED)
+                    {
+                        static char buffer[4096];
+                        uint64_t bytes;
+                        while (cellFsRead(fsrc, buffer, sizeof(buffer), &bytes) == CELL_FS_SUCCEEDED && bytes > 0)
+                        {
+                            uint64_t written;
+                            cellFsWrite(fdst, buffer, bytes, &written);
+                        }
+                        cellFsClose(fdst);
+                    }
+                    cellFsClose(fsrc);
+                }
+                shouldPermit = true;
+            }
+        }
+        else
+        {
+            ::snprintf(contentInfoPath, sizeof(contentInfoPath), "/dev_hdd0/game/%s", init.titleId);
+            shouldPermit = true;
+        }
+
+        if (shouldPermit)
+        {
+            cellSysutilCheckCallback();
+            ret = cellGameContentPermit(contentInfoPath, usrdirPath);
+            if (ret == 0)
+            {
+                ::strncpy(s_savePath, usrdirPath, sizeof(s_savePath));
+                s_savePath[sizeof(s_savePath) - 1] = '\0';
+                ::strcat(s_savePath, "/");
+            }
+        }
+    }
 #else
     // Desktop: all files relative to the working directory.
     s_userPath[0] = '\0';
@@ -83,17 +169,35 @@ bool IsAssetPath(const char *path)
 #endif
     if (dot)
     {
-#ifdef _WIN32
+#if defined(_WIN32) && !defined(__PS3__)
         if (_stricmp(dot, ".dat") == 0)
+        {
+            const char *p = path;
+            const char *lastSep = strrchr(path, '/');
+            if (strrchr(path, '\\') > lastSep) lastSep = strrchr(path, '\\');
+            if (lastSep) p = lastSep + 1;
+            if (_stricmp(p, "score.dat") == 0) return false;
             return true;
-#else
-#ifdef __PS3__
+        }
+#elif defined(__PS3__)
         if (::strlen(dot) == 4 && ::tolower(dot[1]) == 'd' && ::tolower(dot[2]) == 'a' && ::tolower(dot[3]) == 't')
+        {
+            const char *p = path;
+            const char *lastSep = ::strrchr(path, '/');
+            if (::strrchr(path, '\\') > lastSep) lastSep = ::strrchr(path, '\\');
+            if (lastSep) p = lastSep + 1;
+            if (::strcasecmp(p, "score.dat") == 0) return false;
             return true;
+        }
 #else
         if (strcasecmp(dot, ".dat") == 0)
+        {
+            const char *p = path;
+            const char *lastSep = strrchr(path, '/');
+            if (lastSep) p = lastSep + 1;
+            if (strcasecmp(p, "score.dat") == 0) return false;
             return true;
-#endif
+        }
 #endif
     }
 
@@ -113,9 +217,14 @@ void Resolve(char *outBuf, size_t outBufSize, const char *path)
         path += 2;
 
 #ifdef __PS3__
-    // On PS3, we always want to prepend the absolute base path to ensure 
-    // files are found regardless of whether they are "assets" or "user data".
-    ::snprintf(outBuf, outBufSize, "%s%s", s_userPath, path);
+    if (IsAssetPath(path))
+    {
+        ::snprintf(outBuf, outBufSize, "%s%s", s_userPath, path);
+    }
+    else
+    {
+        ::snprintf(outBuf, outBufSize, "%s%s", s_savePath, path);
+    }
 #else
     if (IsAssetPath(path))
     {
